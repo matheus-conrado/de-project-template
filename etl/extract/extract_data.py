35fc177d170e683b0dfe7dde287abaae615fca76
Matheus Conrado <mconrado@pop-os.localdomain>
2023-05-31 23:32:50 -0300

    Include ETL process

---
+++ etl/extract/extract_data.py
@@ -0,0 +1,2 @@
+class Extract():
+    pass
---
+++ pipelines/pipeline.py
@@ -0,0 +1,6 @@
+from ..etl.extract import extract_data
+from ..etl.load import load_data
+from ..etl.transform import transform_data
+
+class Pipeline():
+    pass
